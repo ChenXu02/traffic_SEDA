0fee5f60863a4c81803d1d6742c0487c272cfafc
Chen Xu <126840872+ChenXu02@users.noreply.github.com>
2023-03-07 11:27:46 +0800

    Add files via upload

---
+++ models/predicter/AGCRN/lib/load_dataset.py
@@ -0,0 +1,19 @@
+import os
+import numpy as np
+
+def load_st_dataset(dataset):
+    #output B, N, D
+    if dataset == 'PEMSD4':
+        data_path = os.path.join('../data/PeMSD4/pems04.npz')
+        data = np.load(data_path)['data'][:, :, 0]  #onley the first dimension, traffic flow data
+    elif dataset == 'PEMSD8':
+        data_path = os.path.join('../data/PeMSD8/pems08.npz')
+        data_path_missing = os.path.join('../data/PeMSD8/pems08_pm_random_mat_40%25.npy')
+        data = np.load(data_path)['data'][:, :, 0]  #onley the first dimension, traffic flow data
+        data_missing = np.load(data_path_missing)[:,0,:].transpose(1,0)  # onley the first dimension, traffic flow data
+    else:
+        raise ValueError
+    if len(data.shape) == 2:
+        data = np.expand_dims(data, axis=-1)
+    print('Load %s Dataset shaped: ' % dataset, data.shape, data.max(), data.min(), data.mean(), np.median(data))
+    return data,data_missing
